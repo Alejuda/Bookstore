1343fb30b9ac64f363eb23f1a5c2e05a5bd87fd0
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-06-12 17:06:53 -0300

    Add reducers to add and remove books on the booklist.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,26 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from 'uuid';
 
-const initialState = {
-  books: [
-    {
-      id: uuidv4(),
-      title: 'Book 1',
-      author: 'Author 1',
-      progress: '60%',
-    },
-    {
-      id: uuidv4(),
-      title: 'Book 2',
-      author: 'Author 2',
-      progress: '76%',
-    },
-  ]
-};
+const initialState = [
+  {
+    id: uuidv4(),
+    title: 'Book 1',
+    author: 'Author 1',
+    progress: '60%',
+  },
+  {
+    id: uuidv4(),
+    title: 'Book 2',
+    author: 'Author 2',
+    progress: '76%',
+  },
+];
 
 const booksSlice = createSlice({
   name: "books",
   initialState,
+  reducers: {
+    addBook: (state, action) => {
+      const newBook = {
+        id: uuidv4(),
+        title: action.payload.title,
+        author: action.payload.author,
+        progress: '0%',
+      }
+      state.push(newBook);
+    },
+    removeBook: (state, action) => {
+      const itemId = action.payload.id;
+      state = state.filter(item => item.id !== itemId);
+    }
+  }
 });
 
 export default booksSlice.reducer
