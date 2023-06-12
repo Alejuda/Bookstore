6eccf674bd8e21cfb5a9fbd2d4ab6a08ea308523
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-06-12 18:17:39 -0300

    Fix required changes

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -8,7 +8,11 @@ const initialState = {
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
-  reducers: {},
+  reducers: {
+    checkStatus: (state) => state.status,
+  },
 });
+
+export const { checkStatus } = categoriesSlice.actions;
 
 export default categoriesSlice.reducer;
