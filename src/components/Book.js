43f9dd18a652a474f717c069c29c3b4fc9b44fec
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-06-14 19:27:29 -0300

    Add clases and styles for Header, Footer and AddForm components

--- src/components/Book.js
+++ src/components/Book.js
@@ -11,10 +11,14 @@ const Book = (
 
   return (
     <div className="book-container">
-      <h2>{title}</h2>
-      <p>{author}</p>
-      <p>{category}</p>
-      <button type="button" onClick={() => dispatch(removeBook(itemId))}>Delete</button>
+      <p className="book-category">{category}</p>
+      <h2 className="book-title">{title}</h2>
+      <p className="book-author">{author}</p>
+      <div className="controls-container">
+        <button className="book-control" type="button">Comments</button>
+        <button className="book-control" type="button" onClick={() => dispatch(removeBook(itemId))}>Remove</button>
+        <button className="book-control" type="button">Edit</button>
+      </div>
     </div>
   );
 };
